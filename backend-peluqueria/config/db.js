bdf80ef76d75b803abf3427d3738fa994c2a4c7d
Angelevo09 <angelevo09@gmail.com>
2026-06-14 19:28:05 -0400

    Añadir puerto dinámico y SSL para base de datos

--- backend-peluqueria/config/db.js
+++ backend-peluqueria/config/db.js
@@ -1,12 +1,16 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// Crear el "pool" de conexiones utilizando las variables del archivo .env
+// Crear el "pool" de conexiones utilizando las variables del archivo .env o de Render
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306, // <-- NUEVO: Para leer el puerto especial de Aiven
+    ssl: {                             // <-- NUEVO: Escudo de seguridad obligatorio para la nube
+        rejectUnauthorized: false
+    },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
@@ -19,7 +23,7 @@ const promisePool = pool.promise();
 // Probar la conexión al arrancar
 promisePool.getConnection()
     .then(connection => {
-        console.log('Conexión exitosa a la base de datos MySQL (peluqueria_reservas)');
+        console.log('Conexión exitosa a la base de datos MySQL');
         connection.release(); // Liberamos la conexión para que otros la usen
     })
     .catch(err => {
